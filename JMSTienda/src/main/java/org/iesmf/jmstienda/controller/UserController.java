0d502f5c5ea13a1be0e0dede8bfdc1252241684f
teanma <mardaleteodorandrei@gmail.com>
2022-01-18 11:10:09 +0100

    Update

---
+++ JMSTienda/src/main/java/org/iesmf/jmstienda/controller/UserController.java
@@ -0,0 +1,32 @@
+package org.iesmf.jmstienda.controller;
+
+import org.iesmf.jmstienda.Email;
+import org.iesmf.jmstienda.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class UserController {
+
+    private JmsTemplate jmsTemplate;
+
+    public UserController(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/users")
+    public void insert(@RequestBody User user) {
+        System.out.println(user.toString());
+        Email email = new Email(
+                user.getEmail(),
+                "Bienvenido " + user.getUsername(),
+                "Gracias por registrarte " + user.getUsername()
+        );
+        jmsTemplate.convertAndSend("emails", email);
+    }
+}
---
+++ JMSTienda/src/main/java/org/iesmf/jmstienda/sender/APIMain.java
@@ -0,0 +1,13 @@
+package org.iesmf.jmstienda.sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class APIMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(APIMain.class, args);
+    }
+}
