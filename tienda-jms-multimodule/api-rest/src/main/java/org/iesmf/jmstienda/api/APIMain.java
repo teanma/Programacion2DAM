4a697814142b827c0617a8dc730bf57f80158535
teanma <mardaleteodorandrei@gmail.com>
2022-01-25 10:23:58 +0100

    Update

---
+++ tienda-jms-multimodule/api-rest/src/main/java/org/iesmf/jmstienda/api/APIMain.java
@@ -0,0 +1,12 @@
+package org.iesmf.jmstienda.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APIMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(APIMain.class, args);
+    }
+}
