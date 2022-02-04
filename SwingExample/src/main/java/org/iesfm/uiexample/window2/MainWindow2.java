d82512245440474da301bf5cdea544c2f67ff2f0
teanma <mardaleteodorandrei@gmail.com>
2022-02-04 10:26:38 +0100

    Update

--- SwingExample/src/main/java/org/iesfm/uiexample/window2/MainWindow2.java
+++ SwingExample/src/main/java/org/iesfm/uiexample/window2/MainWindow2.java
@@ -5,10 +5,11 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 public class MainWindow2 extends JFrame {
 
-    private JPanel bookPanel, createBookPanel, modifyBookPanel;
+    private JPanel bookPanel, createBookPanel, modifyBookPanel, askForIsbnPanel;
 
     private JButton createBookButton;
 
@@ -17,6 +18,10 @@ public class MainWindow2 extends JFrame {
     private JLabel booksLabel;
 
     private TextField isbnTextField, titleTextField, authorTextField, yearTextField;
+
+    private JList<String> books;
+
+    private DefaultListModel<String> listModel;
 
     @Override
     protected void frameInit() {
@@ -40,6 +45,9 @@ public class MainWindow2 extends JFrame {
         bookPanel.setBorder(BorderFactory.createTitledBorder("Libros disponibles"));
         bookPanel.setBounds(10, 10, 1045, 600);
         bookPanel.setLayout(new GridLayout(1, 1));
+        listModel = new DefaultListModel<>();
+        books = new JList<>(listModel);
+        bookPanel.add(books);
         //bookPanel.add(getBooksLabel(), BorderLayout.CENTER);
         return bookPanel;
     }
@@ -80,12 +88,22 @@ public class MainWindow2 extends JFrame {
                     String introYear = yearTextField.getText();
                     int year = Integer.parseInt(introYear);
 
-                    System.out.println("ISBN: " + isbn);
+                    /*System.out.println("ISBN: " + isbn);
                     System.out.println("Título: " + title);
                     System.out.println("Autor: " + author);
-                    System.out.println("Año: " + year);
+                    System.out.println("Año: " + year);*/
 
-                    //getBookPanel().setText(title);
+                    /*listModel.addElement("ISBN: " + isbn);
+                    listModel.addElement("Título: " + title);
+                    listModel.addElement("Autor: " + author);
+                    listModel.addElement("Año: " + year);
+                    //listModel.addElement("----------------");*/
+
+                    String book = String.valueOf("ISBN: " + isbn + "\n" + "Título: " + title + "\n" + "Author: " + author + "\n" + "Año: " + year);
+
+                    JOptionPane.showMessageDialog(null, book);
+
+                    /*OptionPane.showMessageDialog(null, "Libro creado");*/
                 } else {
                     JOptionPane.showMessageDialog(null, "Operación cancelada");
                 }
@@ -104,6 +122,13 @@ public class MainWindow2 extends JFrame {
                 authorTextField = new TextField(25);
                 yearTextField = new TextField(25);
 
+                /*askForIsbnPanel = new JPanel();
+
+                askForIsbnPanel.setLayout(new GridLayout(1,2));
+
+                askForIsbnPanel.add(new JLabel("NIF:"));
+                askForIsbnPanel.add(isbnTextField);*/
+
                 modifyBookPanel = new JPanel();
 
                 modifyBookPanel.setLayout(new GridLayout(3, 2));
@@ -117,6 +142,14 @@ public class MainWindow2 extends JFrame {
                 modifyBookPanel.add(new Label("Año:"));
                 modifyBookPanel.add(yearTextField);
 
+                /*int askForIsbn = JOptionPane.showConfirmDialog(null, askForIsbnPanel, "Introducir nif", JOptionPane.OK_CANCEL_OPTION);
+
+                if(askForIsbn == JOptionPane.OK_OPTION) {
+                    String isbn = isbnTextField.getText();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Operación cancelada");
+                }*/
+
                 int reply = JOptionPane.showConfirmDialog(null, modifyBookPanel, "Modificar libro", JOptionPane.OK_CANCEL_OPTION);
 
                 if (reply == JOptionPane.OK_OPTION) {
@@ -125,9 +158,11 @@ public class MainWindow2 extends JFrame {
                     String introYear = yearTextField.getText();
                     int year = Integer.parseInt(introYear);
 
-                    System.out.println("Título: " + title);
-                    System.out.println("Autor: " + author);
-                    System.out.println("Año: " + year);
+                    listModel.addElement("Título: " + title);
+                    listModel.addElement("Autor: " + author);
+                    listModel.addElement("Año: " + year);
+
+                    JOptionPane.showMessageDialog(null, "Libro modificado");
                 } else {
                     JOptionPane.showMessageDialog(null, "Operación cancelada");
                 }
